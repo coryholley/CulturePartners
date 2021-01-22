01cabd3c95f4db201dc1cee0b2ebf40576adb69b
Cory <courtneynholley@gmail.com>
2021-01-22 13:42:22 -0600

    Feat: Setup of the culturePartners project

---
+++ src/main/java/com/example/culturepartners/CulturepartnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.culturepartners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturepartnersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturepartnersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/culturepartners/CulturepartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.culturepartners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturepartnersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
